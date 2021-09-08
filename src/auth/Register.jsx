8806e1d13380d04825ebdd666edf08f9eb505915
Courtney Downs <courtneysdowns@gmail.com>
2021-09-07 21:04:56 -0400

    modified .gitignore

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,57 +1,73 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 const Register = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hey there brother");
+    fetch("http://localhost:3001/user/create", {
+      //1
+      method: "POST",
+      body: JSON.stringify({ user: { email: email, password: password } }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.updateToken(data.sessionToken);
+      }); //ADD CATCH
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('hey there brother')
-        fetch('http://localhost:3001/user/create', { //1
-            method: 'POST',
-            body: JSON.stringify({user: {email: email, password: password}}),
-            headers: new Headers({
-                'Content-Type':'application/json'
-            })
-        }).then(
-            (response) => response.json()
-        ).then((data) => {
-            props.updateToken(data.sessionToken)
-        })
-    }
+  return (
+    <div className="login-register">
+      <h2>This is the Register</h2>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="firstName">First Name</Label>
+          <Input
+            onChange={(e) => setFirstName(e.target.value)}
+            name="firstName"
+            value={firstName}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="lastName">Last Name</Label>
+          <Input
+            onChange={(e) => setLastName(e.target.value)}
+            name="lastName"
+            value={lastName}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={email}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={password}
+          />
+        </FormGroup>
+        <Button type="submit">Signup</Button>
+      </Form>
+      <Link to="/" variant="body2">
+        Already have an account ? Sign in here
+      </Link>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="login-register">
-            <h2>This is the Register</h2>
-            <Form onSubmit={handleSubmit}>
-                <FormGroup>
-                    <Label htmlFor="firstName">First Name</Label>
-                    <Input onChange={(e) => setFirstName(e.target.value)}  name="firstName" value={firstName} />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="lastName">Last Name</Label>
-                    <Input onChange={(e) => setLastName(e.target.value)}  name="lastName" value={lastName} />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="email">Email</Label>
-                    <Input onChange={(e) => setEmail(e.target.value)} name="email" value={email} />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="password">Password</Label>
-                    <Input onChange={(e) => setPassword(e.target.value)}  name="password" value={password} />
-                </FormGroup>
-                <Button type="submit">Signup</Button>
-            </Form>
-            <Link to="/" variant = "body2">
-                Already have an account ? Sign in here 
-            </Link>
-        </div>
-     );
-}
- 
 export default Register;
