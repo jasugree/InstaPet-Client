91dc52abe0cd6d5c12279bf9fc479e4d58be113c
Mitch Strange <mstrange382@gmail.com>
2021-09-10 20:25:48 -0500

    updating like button branch

--- src/posts/PostIndex.jsx
+++ src/posts/PostIndex.jsx
@@ -1,37 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import {Container, Row, Col} from 'reactstrap';
-import Sitebar from '../navbar/Navbar';
-import LikeButton from './LikeButton';
-
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import Sitebar from "../navbar/Navbar";
+import PostFeed from "./PostFeed";
+import PostCreate from "./PostCreate";
 
 const PostIndex = (props) => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    const fetchPosts = () => {
-        fetch('https://localhost:3001/', {
-            method: 'GET',
-            headers: new Headers ({
-                'Content-Type': 'application/json',
-                'Authorization': props.token
-            })
-        }) .then( (res) => res.json ())
-        .then((logData) => {
-            setPosts(logData)
-        })
-    }
+  const fetchPosts = () => {
+    fetch("http://localhost:3001/post", {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((feedData) => {
+        setPosts(feedData);
+      });
+  };
 
-    useEffect(() => {
-        fetchPosts();
-    }, [])
-    
-    return (
-        <div>
-            <Sitebar clickLogout={props.clearToken}/>
-            Hello, PostIndex.
-            <LikeButton />
-            
-        </div>
-    )
-}
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
+  const fetchUsers = () => {
+    fetch("http://localhost:3001/user", {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((UserData) => {
+        setUsers(UserData);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <Sitebar token={props.token} clickLogout={props.clearToken} />
+      <Container>
+        <Row>
+          <Col>
+            <PostFeed posts={posts} fetchUsers={fetchUsers} users={users} fetchPosts={fetchPosts} token={props.token}  />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default PostIndex;
