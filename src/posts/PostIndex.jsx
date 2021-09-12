3478dacf7c4d1be16877c583ecb8325c4aed5017
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-09-12 17:30:20 -0400

    pushing so that I can revert to previous version

--- src/posts/PostIndex.jsx
+++ src/posts/PostIndex.jsx
@@ -1,77 +1,3 @@
-// //
-// import React, { useState, useEffect } from "react";
-// import { Container, Row, Col } from "reactstrap";
-// import Sitebar from "../navbar/Navbar";
-// import PostFeed from "./PostFeed";
-// import PostCreate from "./PostCreate";
-
-// const PostIndex = (props) => {
-//   const [posts, setPosts] = useState(null);
-//   const [users, setUsers] = useState(null);
-
-//   const fetchPosts = () => {
-//     fetch("http://localhost:3001/post", {
-//       method: "GET",
-//       headers: new Headers({
-//         "Content-Type": "application/json",
-//         Authorization: props.token,
-//       }),
-//     })
-//       .then((res) => res.json())
-//       .then((feedData) => {
-//         setPosts(feedData);
-//       });
-//   };
-
-//   useEffect(() => {
-//     fetchPosts();
-//   }, []);
-
-//   const fetchUsers = () => {
-//     fetch("http://localhost:3001/user", {
-//       method: "GET",
-//       headers: new Headers({
-//         "Content-Type": "application/json",
-//         Authorization: props.token,
-//       }),
-//     })
-//       .then((res) => res.json())
-//       .then((UserData) => {
-//         setUsers(UserData);
-//       });
-//   };
-
-//   useEffect(() => {
-//     fetchUsers();
-//   }, []);
-//   console.log(posts);
-//   console.log(users);
-//   return (
-//     <div>
-//       <Sitebar
-//         token={props.token}
-//         fetchPosts={fetchPosts}
-//         clickLogout={props.clearToken}
-//       />
-//       <Container>
-//         <Row>
-//           <Col>
-//             <PostFeed
-//               posts={posts}
-//               fetchUsers={fetchUsers}
-//               users={users}
-//               fetchPosts={fetchPosts}
-//               token={props.token}
-//             />
-//           </Col>
-//         </Row>
-//       </Container>
-//     </div>
-//   );
-// };
-
-// export default PostIndex;
-
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Sitebar from "../navbar/Navbar";
@@ -144,3 +70,76 @@ const PostIndex = (props) => {
 };
 
 export default PostIndex;
+
+// import React, { useState, useEffect } from "react";
+// import { Container, Row, Col } from "reactstrap";
+// import Sitebar from "../navbar/Navbar";
+// import PostFeed from "./PostFeed";
+// import PostCreate from "./PostCreate";
+
+// const PostIndex = (props) => {
+//   const [posts, setPosts] = useState(null);
+//   const [users, setUsers] = useState(null);
+
+//   const fetchPosts = () => {
+//     fetch("http://localhost:3001/post", {
+//       method: "GET",
+//       headers: new Headers({
+//         "Content-Type": "application/json",
+//         Authorization: props.token,
+//       }),
+//     })
+//       .then((res) => res.json())
+//       .then((feedData) => {
+//         setPosts(feedData);
+//       });
+//   };
+
+//   useEffect(() => {
+//     fetchPosts();
+//   }, []);
+
+//   const fetchUsers = () => {
+//     fetch("http://localhost:3001/user", {
+//       method: "GET",
+//       headers: new Headers({
+//         "Content-Type": "application/json",
+//         Authorization: props.token,
+//       }),
+//     })
+//       .then((res) => res.json())
+//       .then((UserData) => {
+//         setUsers(UserData);
+//       });
+//   };
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
+//   console.log(posts);
+//   console.log(users);
+//   return (
+//     <div>
+//       <Sitebar
+//         token={props.token}
+//         fetchPosts={fetchPosts}
+//         clickLogout={props.clearToken}
+//       />
+//       <Container>
+//         <Row>
+//           <Col>
+//             <PostFeed
+//               posts={posts}
+//               fetchUsers={fetchUsers}
+//               users={users}
+//               fetchPosts={fetchPosts}
+//               token={props.token}
+//             />
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default PostIndex;
