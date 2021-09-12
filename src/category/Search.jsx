9b1452a0760a59d77e38decc0cf09e93672e8d60
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-09-12 17:38:05 -0400

    committing so I can revert to a previous version

--- src/category/Search.jsx
+++ src/category/Search.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import React, { useState, useEffect } from "react";
 // // import { InputGroup, InputGroupAddon, Input, Button } from "reactstrap";
 // import "../App.css";
@@ -101,3 +102,52 @@
 // // };
 
 // export default Search;
+=======
+import React, { useState, useEffect } from "react";
+import { InputGroup, InputGroupText, InputGroupAddon, Input } from "reactstrap";
+import "../App.css";
+
+const Search = (props) => {
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Testing, testing. 1, 2, 3.");
+
+    fetch("http:localhost:3001/post/:category", {
+      method: "POST",
+      body: JSON.stringify({
+        post: {
+          category,
+        },
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((logData) => {
+        console.log(logData);
+        setCategory("");
+        Search();
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+  return (
+    <InputGroup
+      className=".z-depth-1-half mx-auto"
+      style={{ display: "flex", borderRadius: 6, width: 300 }}
+    >
+      <Input placeholder="Hashtag search" />
+      <InputGroupAddon addonType="append">
+        <InputGroupText>Search</InputGroupText>
+      </InputGroupAddon>
+    </InputGroup>
+  );
+};
+
+export default Search;
+>>>>>>> parent of 26fa927 (restarted Search.jsx code. Needs debugging.)
